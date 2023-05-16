f4c2c8d000aae5fbec73faa74ed7eb530aea9b92
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-16 14:41:38 +0330

    initial commit

--- app/src/main/java/com/ghores/digikalacompose/MainActivity.kt
+++ app/src/main/java/com/ghores/digikalacompose/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.ghores.digikalacompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.ghores.digikalacompose.ui.theme.DigikalaComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DigikalaComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+
+                    }
                 ) {
 
                 }
