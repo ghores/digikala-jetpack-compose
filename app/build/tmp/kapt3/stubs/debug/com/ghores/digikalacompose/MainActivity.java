735630ade50ce0c2b8dc4c9476a2945f6a8f0953
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-16 15:29:15 +0330

    navController pages

--- app/build/tmp/kapt3/stubs/debug/com/ghores/digikalacompose/MainActivity.java
+++ app/build/tmp/kapt3/stubs/debug/com/ghores/digikalacompose/MainActivity.java
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.activity.ComponentActivity;
 import androidx.compose.material3.ExperimentalMaterial3Api;
-import androidx.compose.ui.Modifier;
-import androidx.navigation.NavController;
 import androidx.navigation.NavHostController;
 
 @kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0005\u001a\u00020\u00062\b\u0010\u0007\u001a\u0004\u0018\u00010\bH\u0015R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\t"}, d2 = {"Lcom/ghores/digikalacompose/MainActivity;", "Landroidx/activity/ComponentActivity;", "()V", "navController", "Landroidx/navigation/NavHostController;", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
