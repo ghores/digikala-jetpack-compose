091a70b0a7db7492a64298caf5c6b30b36a0fc6b
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-16 23:00:21 +0330

    add fonts

--- app/src/main/java/com/ghores/digikalacompose/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/ghores/digikalacompose/navigation/BottomNavigationBar.kt
@@ -3,14 +3,16 @@ package com.ghores.digikalacompose.navigation
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -64,7 +66,7 @@ fun BottomNavigationBar(
             backgroundColor = Color.White,
             elevation = 5.dp
         ) {
-            items.forEachIndexed { index, item ->
+            items.forEachIndexed { _, item ->
                 val selected = item.route == backStackEntry.value?.destination?.route
                 BottomNavigationItem(selected = selected, onClick = { onItemClick(item) },
                     selectedContentColor = MaterialTheme.colors.selectedBottomBar,
@@ -87,7 +89,7 @@ fun BottomNavigationBar(
                             Text(
                                 text = item.name,
                                 textAlign = TextAlign.Center,
-                                style = androidx.compose.material3.MaterialTheme.typography.headlineSmall,
+                                style = androidx.compose.material3.MaterialTheme.typography.titleSmall,
                                 fontWeight = FontWeight.Bold,
                                 modifier = Modifier.padding(top = 5.dp)
                             )
--- app/src/main/java/com/ghores/digikalacompose/ui/theme/Type.kt
+++ app/src/main/java/com/ghores/digikalacompose/ui/theme/Type.kt
@@ -1,23 +1,83 @@
 package com.ghores.digikalacompose.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ghores.digikalacompose.R
+
+val font_medium = FontFamily(
+    Font(R.font.iranyekanmedium)
+)
+val font_bold = FontFamily(
+    Font(R.font.iranyekanbold)
+)
+val font_standard = FontFamily(
+    Font(R.font.iranyekan)
+)
+
+val Typography.extraBoldNumber: TextStyle
+    @Composable
+    get() = TextStyle(
+        fontFamily = font_bold,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp
+    )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = font_medium,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
+        lineHeight = 25.sp,
         letterSpacing = 0.5.sp
     ),
-    headlineSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = font_standard,
         fontWeight = FontWeight.Medium,
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        lineHeight = 25.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 25.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 25.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        lineHeight = 25.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 25.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 25.sp
     )
 )
